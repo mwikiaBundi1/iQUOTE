4e3dce7b2e43fd9a960b6d427b8d335393873256
{mwikiaBundi1} <{mwikiam@gmail.com}>
2020-03-31 12:05:22 +0300

    add a list of quote ids and names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Quote} from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,10 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  quotes:string[];
-
-  constructor(){
-    this.quotes = ['I would rather be an artist than a leader', 'Better late than never', 'Unity is strength']
-  } 
+  quotes:Quote[] = [
+    {id:1, name:'I would rather be an artist than a leader'},
+    {id:2,name:'Better late than never'},
+    {id:3,name:'Unity is strength'},
+    {id:4,name:'Change begins with one person'},
+    {id:5,name:'Worry what your child hear from you'},
+  ]; 
 }
 
