6de2c9711367e8c3e168cb23be43e75818293408
{mwikiaBundi1} <{mwikiam@gmail.com}>
2020-04-01 11:55:14 +0300

    add delete quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -17,6 +17,13 @@ export class QuoteComponent implements OnInit {
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
+
+  completeGoal(isComplete, index){
+    if (isComplete) {
+      this.quotes.splice(index,1);
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
